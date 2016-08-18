60190194ba1878f7f705df3e46d18b2cccbf281d
Rashmi Sakleshpur Nagaraju <rasn@cisco.com>
2016-08-18 13:40:43 -0700

    SingleLinkedList - Reverse from position M to N

--- src/lists/singlyLinkedList/Main.java
+++ src/lists/singlyLinkedList/Main.java
@@ -10,5 +10,12 @@ public class Main {
         ReverseSinglyLinkedList reverseSinglyLinkedList = new ReverseSinglyLinkedList();
         head = reverseSinglyLinkedList.reverse(head);
         System.out.println("Reversed list = " + head);
+
+        ListNode head2 = ListNode.createList();
+        head2.next.next.next.next = new ListNode(5);
+        System.out.println("Original List  = " + head2);
+        ReverseSinglyLinkedListPositionMtoN reverse = new ReverseSinglyLinkedListPositionMtoN();
+        head2 = reverse.reverse(head2, 1, 3);
+        System.out.println("head2 = " + head2);
     }
 }
